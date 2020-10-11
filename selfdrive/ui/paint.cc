1913fcd313c83a1455f036cd2f8df759c9bd90d7
neokii <neokii@gmail.com>
2020-10-11 22:31:56 +0900

    added mpc track and brake image to UI

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -927,13 +927,13 @@ static void ui_draw_driver_view(UIState *s) {
 static void ui_draw_vision_brake(UIState *s) {
   const UIScene *scene = &s->scene;
   const int brake_size = 96;
-  const int brake_x = (scene->ui_viz_rx + (brake_size * 5) + (bdr_is * 4));
-  const int brake_y = (footer_y + ((footer_h - brake_size) / 2));
+  const int brake_x = (s->scene.viz_rect.x + (brake_size * 5) + (bdr_is * 4));
+  const int brake_y = (s->scene.viz_rect.bottom() + ((footer_h - brake_size) / 2));
   const int brake_img_size = (brake_size * 1.5);
   const int brake_img_x = (brake_x - (brake_img_size / 2));
   const int brake_img_y = (brake_y - (brake_size / 4));
 
-  bool brake_valid = scene->brakeLights;
+  bool brake_valid = scene->car_state.getBrakeLights();
   float brake_img_alpha = brake_valid ? 1.0f : 0.15f;
   float brake_bg_alpha = brake_valid ? 0.3f : 0.1f;
   NVGcolor brake_bg = nvgRGBA(0, 0, 0, (255 * brake_bg_alpha));
