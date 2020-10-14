a3c393682a0842127a8b5fc7e2af08dd843c359e
stonerains <72440538+stonerains@users.noreply.github.com>
2020-10-14 12:05:45 +0900

    Update lane_planner.py
    추가 가중치

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -44,7 +44,9 @@ def calc_d_poly(l_poly, r_poly, p_poly, l_prob, r_prob, lane_width, v_ego):
 
   # neokii
   if lr_prob > 0.7:
-    lr_prob = min(lr_prob * 1.3, 1.0)
+    lr_prob = min(lr_prob * 1.33, 1.0)
+  elif lr_prob > 0.5:
+    lr_prob = min(lr_prob * 1.2, 1.0)
 
   d_poly_lane = (l_prob * path_from_left_lane + r_prob * path_from_right_lane) / (l_prob + r_prob + 0.0001)
   return lr_prob * d_poly_lane + (1.0 - lr_prob) * p_poly
